080925fcf39e50d9001f6490d1c17e46892c3701
Halston <sellenth@oregonstate.edu>
2020-05-20 09:55:21 -0700

    a few comments

--- src/lib.ts
+++ src/lib.ts
@@ -2,116 +2,166 @@ import { createReadStream } from "fs";
 import { createInterface } from "readline";
 
 export class NaiveBayes {
-  private _trainingData: string[][] = []
-  private _testingData: string[][] = []
+  private _trainingData: string[][] = []; // the cleaned up and word separated training data
+  private _testingData: string[][] = []; // the cleaned up and word separated testing data
 
-  private _trainLabels: number[] = []
-  private _testLabels: number[] = []
-  private _dictionary = new Map()
+  private _trainLabels: number[] = []; // array for each example's sentiment 1 (positive) or 0 (negative)
+  private _testLabels: number[] = []; // array for each example's sentiment 1 (positive) or 0 (negative)
+  private _dictionary = new Map(); // all unique words found in the training data
 
-  private _featurizedTrain: number[][] = []
-  private _featurizedTest: number[][] = []
-  private _featurizedPositives: number[][] = []
-  private _featurizedNegatives: number[][] = []
+  private _featurizedTrain: number[][] = []; // all examples from training data (featurized)
+  private _featurizedTest: number[][] = []; // all examples from testing data (featurized)
+  private _featurizedPositives: number[][] = []; // only the positive examples from training data
+  private _featurizedNegatives: number[][] = []; // only the negative examples from training data
 
-  private _numExamples: number = 0
-  private _numPos: number = 0
-  private _numNeg: number = 0
-  private _posPos: number[] = []
-  private _posNeg: number[] = []
-  private _negPos: number[] = []
-  private _negNeg: number[] = []
+  private _numExamples: number = 0; // total number of examples in training data
+  private _numPos: number = 0; // number of positive examples in training data
+  private _numNeg: number = 0; // number of negative examples in training data
+  private _posPos: number[] = []; // tally when dictionary word occurred & example was positive 
+  private _posNeg: number[] = []; // tally when dictionary word occurred & example was negative 
+  private _negPos: number[] = []; // tally when dictionary word didn't occurr & example was positive 
+  private _negNeg: number[] = []; // tally when dictionary word didn't occurr & example was positive 
+
 
   constructor(trainFilename: string, testFilename: string, cb: () => void) {
     Promise.all([
+      // wait for the files to be loaded into memory
       this.loadTrainingData(trainFilename),
       this.loadTestingData(testFilename),
     ]).then((_) => {
+      // and then begin training the classifier
       this.makeLabels(this._trainingData, this._trainLabels);
       this.makeLabels(this._testingData, this._testLabels);
 
       this.makeDictionary();
 
-      this.featurizeAll(this._trainingData, this._trainLabels, this._featurizedTrain)
-      this.featurizeAll(this._testingData, this._testLabels, this._featurizedTest)
+      // featurize the training data
+      this.featurizeAll(
+        this._trainingData,
+        this._trainLabels,
+        this._featurizedTrain
+      );
 
-      this.learnBayes(this._featurizedTrain)
+      // featurize the testing data
+      this.featurizeAll(
+        this._testingData,
+        this._testLabels,
+        this._featurizedTest
+      );
 
-      console.log("The accuracy for the training set is:",
-        this.calculateAccuracy(this._featurizedTrain).toFixed(2) + '%');
-      console.log("The accuracy for the testing set is:",
-        this.calculateAccuracy(this._featurizedTest).toFixed(2) + '%');
-    
+      this.learnBayes(this._featurizedTrain);
+
+      console.log(
+        "The accuracy for the training set is:",
+        this.calculateAccuracy(this._featurizedTrain).toFixed(2) + "%"
+      );
+      console.log(
+        "The accuracy for the testing set is:",
+        this.calculateAccuracy(this._featurizedTest).toFixed(2) + "%"
+      );
+
       cb();
     });
   }
 
-  hypothesizeInput(inputStr: string){
-    const parsedInput = this.parseLine(inputStr) 
-    const featurizedInput = this.makeFeatureList(parsedInput, -1)
-    const hypothesis = this.classify(featurizedInput)
-    return hypothesis
-  }
-
-  calculateAccuracy(featurizedData: number[][]){
-      let correct = 0;
-      featurizedData.forEach((example) => {
-          let hypothesis = this.classify(example)
-          if ( hypothesis === example[example.length - 1]){
-              correct += 1;
-          }
+  // loads a file into memory, streaming it line by line
+  // file must exist in the ../assets folder
+  loadFileIntoArr(filename: string, arr: Array<string[]>) {
+    return new Promise((resolve, reject) => {
+      createInterface({
+        input: createReadStream(__dirname + "/../" + filename),
+        terminal: false,
       })
-      return (correct / featurizedData.length * 100)
+        .on("line", (line) => {
+          arr.push(this.parseLine(line));
+        })
+        .on("close", () => {
+          resolve();
+        })
+        .on("SIGINT", () => {
+          reject();
+        });
+    });
   }
 
-  classify(featurizedExample: number[]){
-      let sumClass0: number = Math.log(this._numNeg / this._numExamples)
-      let sumClass1: number = Math.log(this._numPos / this._numExamples)
+  // remove all punctuation from a string and turn
+  // each character lowercase for uniformity
+  // finally, split the string into its individual words
+  parseLine(line: string) {
+    const parsed = line
+      .toLowerCase() // 
+      .replace(/[^\w\s]/g, "")
+      .split(/[ \t]+/);
+    parsed.pop();
+    return parsed;
+  }
 
-      try{
-        featurizedExample.slice(0,-1).forEach((_, idx) => {
-            switch(featurizedExample[idx]){
-                case 0: 
-                    sumClass0 += this._negNeg[idx];
-                    sumClass1 += this._negPos[idx];
-                    break;
-                case 1:
-                    sumClass0 += this._posNeg[idx];
-                    sumClass1 += this._posPos[idx];
-                    break;
-                default:
-                    throw "Label error in data (expect 0/1)"
-            }
-        })
-      } catch(err){
-          console.error(err);
-          console.log("Try cleaning up your featurized data")
-          process.exit(1)
-      }
+  hypothesizeInput(inputStr: string) {
+    const parsedInput = this.parseLine(inputStr);
+    const featurizedInput = this.makeFeatureList(parsedInput, -1);
+    const hypothesis = this.classify(featurizedInput);
+    return hypothesis;
+  }
 
-      if (sumClass1 > sumClass0) {
-          return 1;
-      } else {
-          return 0;
+  calculateAccuracy(featurizedData: number[][]) {
+    let correct = 0;
+    featurizedData.forEach((example) => {
+      let hypothesis = this.classify(example);
+      if (hypothesis === example[example.length - 1]) {
+        correct += 1;
       }
+    });
+    return (correct / featurizedData.length) * 100;
+  }
+
+  classify(featurizedExample: number[]) {
+    let sumClass0: number = Math.log(this._numNeg / this._numExamples);
+    let sumClass1: number = Math.log(this._numPos / this._numExamples);
+
+    try {
+      featurizedExample.slice(0, -1).forEach((_, idx) => {
+        switch (featurizedExample[idx]) {
+          case 0:
+            sumClass0 += this._negNeg[idx];
+            sumClass1 += this._negPos[idx];
+            break;
+          case 1:
+            sumClass0 += this._posNeg[idx];
+            sumClass1 += this._posPos[idx];
+            break;
+          default:
+            throw "Label error in data (expect 0/1)";
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      console.log("Try cleaning up your featurized data");
+      process.exit(1);
     }
 
-  tallyOccurances(featurizedData: number[][], occuranceArr: number[]){
-      featurizedData.forEach((_, idx) => {
-          let jdx = 0;
-          this._dictionary.forEach(() => {
-              occuranceArr[jdx] += featurizedData[idx][jdx]
-              jdx++;
-          })
-      })
+    if (sumClass1 > sumClass0) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
-  learnBayes(featurizedData: number[][]){
-    featurizedData.forEach(featurizedExample => {
-        featurizedExample[featurizedExample.length - 1] === 1 ?
-            this._featurizedPositives.push(featurizedExample.slice(0,-1)) :
-            this._featurizedNegatives.push(featurizedExample.slice(0,-1)) ;
-    })
+  tallyOccurances(featurizedData: number[][], occuranceArr: number[]) {
+    featurizedData.forEach((_, idx) => {
+      let jdx = 0;
+      this._dictionary.forEach(() => {
+        occuranceArr[jdx] += featurizedData[idx][jdx];
+        jdx++;
+      });
+    });
+  }
+
+  learnBayes(featurizedData: number[][]) {
+    featurizedData.forEach((featurizedExample) => {
+      featurizedExample[featurizedExample.length - 1] === 1
+        ? this._featurizedPositives.push(featurizedExample.slice(0, -1))
+        : this._featurizedNegatives.push(featurizedExample.slice(0, -1));
+    });
 
     this._numPos = this._featurizedPositives.length;
     this._numNeg = this._featurizedNegatives.length;
@@ -121,41 +171,49 @@ export class NaiveBayes {
     const negOccurance: number[] = [];
 
     this._dictionary.forEach(() => {
-        posOccurance.push(0);
-        negOccurance.push(0);
-    })
+      posOccurance.push(0);
+      negOccurance.push(0);
+    });
 
     this.tallyOccurances(this._featurizedPositives, posOccurance);
     this.tallyOccurances(this._featurizedNegatives, negOccurance);
 
     posOccurance.forEach((_, idx) => {
-        this._posPos.push(Math.log((posOccurance[idx] + 1) / (this._numPos + 2)))
-        this._negPos.push(Math.log(1 - ((posOccurance[idx] + 1) / (this._numPos + 2)) ))
-    })
+      this._posPos.push(Math.log((posOccurance[idx] + 1) / (this._numPos + 2)));
+      this._negPos.push(
+        Math.log(1 - (posOccurance[idx] + 1) / (this._numPos + 2))
+      );
+    });
 
     negOccurance.forEach((_, idx) => {
-        this._posNeg.push(Math.log((negOccurance[idx] + 1) / (this._numNeg + 2)))
-        this._negNeg.push(Math.log(1 - ((negOccurance[idx] + 1) / (this._numNeg + 2)) ))
-    })
+      this._posNeg.push(Math.log((negOccurance[idx] + 1) / (this._numNeg + 2)));
+      this._negNeg.push(
+        Math.log(1 - (negOccurance[idx] + 1) / (this._numNeg + 2))
+      );
+    });
   }
 
-  makeFeatureList(example: string[], exampleLabel: number){
-      const features: number[] = []
-      this._dictionary.forEach((_, key: string) => {
-          if (example.includes(key)){
-            features.push(1);
-          } else {
-            features.push(0);
-          }
-      } )
-      features.push(exampleLabel)
-      return features;
+  makeFeatureList(example: string[], exampleLabel: number) {
+    const features: number[] = [];
+    this._dictionary.forEach((_, key: string) => {
+      if (example.includes(key)) {
+        features.push(1);
+      } else {
+        features.push(0);
+      }
+    });
+    features.push(exampleLabel);
+    return features;
   }
 
-  featurizeAll(exampleSet: string[][], labels: number[], featurizedDataArr: number[][]){
+  featurizeAll(
+    exampleSet: string[][],
+    labels: number[],
+    featurizedDataArr: number[][]
+  ) {
     exampleSet.forEach((example: string[], idx) => {
-        featurizedDataArr.push(this.makeFeatureList(example, labels[idx]))
-    })
+      featurizedDataArr.push(this.makeFeatureList(example, labels[idx]));
+    });
   }
 
   makeDictionary() {
@@ -168,33 +226,9 @@ export class NaiveBayes {
     });
   }
 
-  parseLine(line: string) {
-    const parsed = line
-      .toLowerCase()
-      .replace(/[^\w\s]/g, "")
-      .split(/[ \t]+/);
-    parsed.pop();
-    return parsed;
-  }
-
   makeLabels(exampleArr: string[][], labelArr: number[]) {
     exampleArr.forEach((example: string[]) => {
       labelArr.push(parseInt(example.pop()));
-    });
-  }
-
-  loadFileIntoArr(filename: string, arr: Array<string[]>) {
-    return new Promise((resolve) => {
-      createInterface({
-        input: createReadStream(__dirname + "/../" + filename),
-        terminal: false,
-      })
-        .on("line", (line) => {
-          arr.push(this.parseLine(line));
-        })
-        .on("close", () => {
-          resolve();
-        });
     });
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { NaiveBayes } from './lib';
 
-let a = new NaiveBayes('assets/trainingSet.txt', 'assets/testSet.txt', () => {
-    console.log(a.hypothesizeInput("I like this place"))
+const prompt = require('prompt-sync')({sigint: true})
+
+const naiveBayes = new NaiveBayes('assets/trainingSet.txt', 'assets/testSet.txt', () => {
+
+    console.log('\nEnter in your input and the classifier will predict whether the string is positive or negative.')
+    console.log('Enter an empty string to quit')
+
+    while(true){
+        let userInput = prompt('\n>> ')
+        if (userInput === '') { process.exit(0) }
+
+        console.log('\nYour review is predicted to be',
+            naiveBayes.hypothesizeInput(userInput) ? 'positive.' : 'negative.')
+    }
 });
